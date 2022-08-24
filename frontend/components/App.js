806aae3492a86efec89011c08152dd1a232da474
Ladrillo <gabriel@cabrejas.net>
2022-08-24 18:40:13 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -11,7 +11,7 @@ const loginUrl = 'http://localhost:9000/api/login'
 
 export default function App() {
   const [articles, setArticles] = useState([])
-  const [] = useState(null)
+  const [currentArticleId, setCurrentArticleId] = useState(null)
 
   const navigate = useNavigate()
 
