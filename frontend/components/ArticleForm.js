78c937d650fde1d5cc9943efaa5c9a2c7f9ccc20
Ladrillo <gabriel@cabrejas.net>
2022-08-24 18:56:01 +0200

    committing to "lecture"

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -22,7 +22,9 @@ export default function ArticleForm(props) {
   }
 
   useEffect(() => {
-    
+    if (currentArticle) {
+      setValues(currentArticle)
+    }
   }, [currentArticle])
 
   return (
