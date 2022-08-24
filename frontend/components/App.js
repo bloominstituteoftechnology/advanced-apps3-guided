eb9618f007f8286c1bc8f36d2a4a7c4a2bb3452b
Ladrillo <gabriel@cabrejas.net>
2022-08-24 18:46:08 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -77,6 +77,7 @@ export default function App() {
           <>
             <ArticleForm
               postArticle={postArticle}
+              currentArticle={/* currentArticlId */}
             />
             <Articles
               articles={articles}
