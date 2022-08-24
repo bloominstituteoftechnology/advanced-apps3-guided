0c2a0e380b76df7e27d817ee817007841b1520d6
Ladrillo <gabriel@cabrejas.net>
2022-08-24 19:21:07 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -68,7 +68,7 @@ export default function App() {
     axiosWithAuth().put(`${articlesUrl}/${article_id}`, article)
       .then(res => {
         const updatedArticle = res.data.article
-        setArticles(articles.map(art => ))
+        setArticles(articles.map(art => art.article_id === article_id ?  : ))
       })
       .catch(err => {
         debugger
