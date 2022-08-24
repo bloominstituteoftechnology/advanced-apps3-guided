43b8b41eae4bfff275a42b6dc583dba6ef55b41c
Ladrillo <gabriel@cabrejas.net>
2022-08-24 18:41:36 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -82,6 +82,7 @@ export default function App() {
               articles={articles}
               getArticles={getArticles}
               deleteArticle={deleteArticle}
+              setCurrentArticleId={setCurrentArticleId}
             />
           </>
         } />
--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 
 export default function Articles(props) {
-  const { articles, getArticles, deleteArticle } = props
+  const { articles, getArticles, deleteArticle, setCurrentArticleId } = props
 
   if (!localStorage.getItem('token')) {
     return <Navigate to="/" />
